21b702813dd958ff516eb3f9e565f4a209d1927c
K <kvannote@mcad.edu>
2023-04-09 23:05:16 -0500

    sketch 2 | assignment 11.a

--- wk11/assignment11.a/sketch2/sketch2.js
+++ wk11/assignment11.a/sketch2/sketch2.js
@@ -1,17 +1,19 @@
+var x = []
+
 function setup() {
     createCanvas(600, 600);
-    background(255,255,255);
-    angleMode(DEGREES);
+    noStroke();
+    fill(238, 130, 238, 225);
+    for (var i = 0; i < 4000; i++) {
+        x[i] = random(-2000, 1000);
+    }
 }
 
-function makeArm(rotateBy) {
-    let alt = Math.round(rotateBy / 45);
-    console.log(alt);
-    stroke('white');
-    noFill();
-    strokeWeight(3);
-    triangle(50, 200, 200, 50, 100, 400);
-
-function mousePressed() {
-    noLoop();
-   }
+function draw() {
+    background(0);
+    for (var i = 0; i < x.length; i++) {
+        x[i] += 2.5;
+        var y = i * 0.4;
+        ellipse(x[i], y, 20, 20);
+    }
+}
