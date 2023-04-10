e47fb3b4e3251aef7622545dc6dea5814208d793
K <kvannote@mcad.edu>
2023-04-09 23:38:46 -0500

    sketch 3 | 11.a assignment

--- wk11/assignment11.a/sketch3/sketch3.js
+++ wk11/assignment11.a/sketch3/sketch3.js
@@ -1,4 +1,22 @@
+var angle = 0.5;
+var offset = 275;
+var scalar = 0;
+var speed = .5;
+
 function setup() {
     createCanvas(600, 600);
-    background(255,255,255);
+    fill(255, 99, 71);
+    background(0);
+}
+
+function draw() {
+    var x = offset + cos(angle) * scalar;
+    var y = offset + sin(angle) * scalar;
+    rect(x, y, 15, 15);
+    angle += speed * 40;
+    scalar += speed;
+}
+
+function mousePressed() {
+    noLoop();
 }
